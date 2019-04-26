089d928a40ca1fb0c739a026bd228fca3dece5ce
Mobile Quickie <mobilequickie@gmail.com>
2019-04-25 22:52:22 -0700

    Updated LWA Proxy with getAccessToken and getUserProfile

--- AuthLWA/LoginWithAmazonProxy.swift
+++ AuthLWA/LoginWithAmazonProxy.swift
@@ -1,12 +1,10 @@
-//
 //  LoginWithAmazonProxy.swift
-//  AuthLWA
 //
-//  Created by Hills, Dennis on 11/1/18.
-//  Copyright © 2018 Hills, Dennis. All rights reserved.
+//  Created by Hills, Dennis on 4/26/19.
+//  Copyright © 2019 Hills, Dennis. All rights reserved.
 //
-//  Credit to SUPERDASHENG at https://superdasheng.wordpress.com/author/superdasheng/
-//
+//  Original credit to SUPERDASHENG at https://superdasheng.wordpress.com/author/superdasheng/
+//  Added logout, getAccessToken, and getUserProfile
 //  https://gist.github.com/mobilequickie/56916503a41ebb2374fea241ede26eab
 
 import Foundation
@@ -16,7 +14,31 @@ class LoginWithAmazonProxy {
     
     static let sharedInstance = LoginWithAmazonProxy()
     
+    // The result of this API requestDidSucceed: callback for a successful login OR
+    // sent to requestDidFail: callback if the login failed
     func login(delegate: AIAuthenticationDelegate) {
-        AIMobileLib.authorizeUser(forScopes: ["profile"], delegate: delegate)
+        AIMobileLib.authorizeUser(forScopes: ["profile", "postal_code"], delegate: delegate)
+    }
+    
+    func logout(delegate: AIAuthenticationDelegate) {
+        AMZNAuthorizationManager.shared().signOut { (error) in
+            if((error) != nil) {
+                print("error signing out from LWA: \(String(describing: error))")
+            }
+            // Deletes cached user tokens and other data from the keychain.
+            // .signOut should be doing this, redundant?
+            AIMobileLib.clearAuthorizationState(delegate)
+        }
+    }
+    
+    // The result of this API is a new access token sent to requestDidSucceed: callback
+    func getAccessToken(delegate: AIAuthenticationDelegate) {
+        AIMobileLib.getAccessToken(forScopes: ["profile"], withOverrideParams: nil, delegate: delegate)
+    }
+
+    // The result of this API is a user profile as an NSDictionary sent to requestDidSucceed: callback
+    // e.g. email = "user@domain.com";name = "first last"; "postal_code" = "xxxxx"; "user_id" = "amzn1.account.xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    func getUserProfile(delegate: AIAuthenticationDelegate) {
+        AIMobileLib.getProfile(delegate)
     }
 }
